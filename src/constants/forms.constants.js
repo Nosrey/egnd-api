5a3dfb221342ef919a46fbc40a9339e8a11b2474
LuanaYellow <luana@yellowpatito.com>
2023-03-06 10:02:12 -0300

    form volumen q works

---
+++ src/constants/forms.constants.js
@@ -0,0 +1,52 @@
+export const MESES = {
+    enero: 0, febrero: 0, marzo: 0, abril: 0, mayo: 0, junio: 0, julio: 0, agosto: 0, septiembre: 0, octubre: 0, noviembre: 0, diciembre: 0,
+}
+export const MONTHS = [
+    'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre',
+]
+
+export const AÑOS = [{
+    año: 1,
+    volMeses: {...MESES},
+    volTotal: 0,
+},
+{
+    año: 2,
+    volMeses: {...MESES},
+    volTotal: 0,
+},
+{
+    año: 3,
+    volMeses: {...MESES},
+    volTotal: 0,
+},{
+    año: 4,
+    volMeses: {...MESES},
+    volTotal: 0,
+},{
+    año: 5,
+    volMeses: {...MESES},
+    volTotal: 0,
+},{
+    año: 6,
+    volMeses: {...MESES},
+    volTotal: 0,
+},{
+    año: 7,
+    volMeses: {...MESES},
+    volTotal: 0,
+},{
+    año: 8,
+    volMeses: {...MESES},
+    volTotal: 0,
+},
+{
+    año: 9,
+    volMeses: {...MESES},
+    volTotal: 0,
+},{
+    año: 10,
+    volMeses: {...MESES},
+    volTotal: 0,
+}
+]
