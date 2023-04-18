2004c1a7dda04051453748c777980e30c41c228c
IgnacioVivas <ignacab@hotmail.com>
2023-04-18 15:22:14 -0300

    actualizando rama

--- src/components/template/Header.js
+++ src/components/template/Header.js
@@ -2,15 +2,35 @@ import React, { useEffect } from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { HEADER_HEIGHT_CLASS } from 'constants/theme.constant'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setFalse } from 'store/icon/iconSlice'
 
 const Header = (props) => {
     const { headerStart, headerEnd, headerMiddle, className, container } = props
+    const currentCurrency = useSelector((state) => state.auth.user.currency)
 
     const dispatch = useDispatch()
     const handleClick = () => {
         dispatch(setFalse())
+    }
+
+    let flagComponent
+
+    switch (currentCurrency) {
+        case '$':
+            flagComponent = <img src="../../assets/image/flag-argentina.png" />
+            break
+        case 'US$':
+            flagComponent = <img src="../../assets/image/flag-eeuu.png" />
+            break
+        case '€':
+            flagComponent = (
+                <img src="../../assets/image/flag-union-europea.png" />
+            )
+            break
+        default:
+            flagComponent = null
+            break
     }
 
     return (
@@ -28,13 +48,16 @@ const Header = (props) => {
                 >
                     {headerStart}
                 </div>
-                {headerMiddle && (
-                    <div className="header-action header-action-middle">
-                        {headerMiddle}
+                <div className="flex items-center">
+                    <div>{flagComponent}</div>
+                    {headerMiddle && (
+                        <div className="header-action header-action-middle">
+                            {headerMiddle}
+                        </div>
+                    )}
+                    <div className="header-action header-action-end">
+                        {headerEnd}
                     </div>
-                )}
-                <div className="header-action header-action-end">
-                    {headerEnd}
                 </div>
             </div>
         </header>
