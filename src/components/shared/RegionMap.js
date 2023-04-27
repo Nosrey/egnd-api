fea121c876c6ff6ca2fc3cff904656ad1adad2a9
LuanaYellow <luana@yellowpatito.com>
2023-04-27 16:43:19 -0300

    fixeado todo

--- src/components/shared/RegionMap.js
+++ src/components/shared/RegionMap.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/default-props-match-prop-types */
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
