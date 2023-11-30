9af1e5f634d37e8eeec5d658f800a9408c0ab79d
Luana andrada <luana@yellowpatito.com>
2023-11-30 17:39:48 -0300

    style spinner

--- src/components/shared/loaders/MySpinner.jsx
+++ src/components/shared/loaders/MySpinner.jsx
@@ -4,7 +4,7 @@ import { Spinner } from "components/ui";
 function MySpinner() {
 
   return (
-    <div className="flex flex-auto flex-col h-[100vh] w-full justify-center items-center">
+    <div className="flex flex-auto flex-col h-[100vh] w-full justify-center items-center ml-[auto] mr-[auto]">
         <Spinner className="mr-4" size="40px" />
     </div> 
   );
